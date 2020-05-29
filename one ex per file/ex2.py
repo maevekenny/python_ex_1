dd6b1023b852d55e61f2a38074cdf590c225e4b1
Maeve Kenny <maevekenny210@gmail.com>
2020-05-29 16:31:30 -0400

    problem two done

--- one ex per file/ex2.py
+++ one ex per file/ex2.py
@@ -8,7 +8,10 @@
 print("start of part 2") # set breakpoint here
 s = "Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects."
 # your code here
-
+sentences = s.split(". ")
+print("There are ", len(sentences), "sentences:")
+for sentence in sentences:
+    print(sentence)    
 print("end of 2") # set breakpoint here 
 '''
 
